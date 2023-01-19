783b468ff933d0b957c4127f2cae2d074959542a
Chibuike Mba <chibuike.code@gmail.com>
2023-01-19 15:58:40 +0100

    Modify investment.c

--- investment/investment.c
+++ investment/investment.c
@@ -2,35 +2,31 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>
-#include "variables.h"
-#include "functions.h"
+#include "../header_files/variables.h"
+#include "../header_files/functions.h"
 
 /**
  * main - Program entry point
  *
- * Description: Prints interactive texts and takes in data needed for calling
- * for the execution of other functions.
+ * Description: Prints the company's header and footer texts to the console and
+ * then calls the feedBack function which in turn activates a chain reaction of
+ * calling other function to carry out their respective designated tasks
  *
  * Return: Returns zero exit status if successfully executed
  */
 int main(void)
 {
-	do {
-		printf("\n- - - - - - - - - - - -Welcome To Stock Investment");
-		printf("Company- - - - - - - - - - - -\n\n");
+	printf("\n- - - - - - - - - - - - -WELCOME TO STOCK INVESTMENT");
+	printf(" COMPANY- - - - - - - - - - - - - -\n\n");
 
-		printf("Warning: Stock investments might sometimes become ");
-		printf("bearish with some permanent loss.\nHaving known this,");
-		printf(" do you still want to invest? (\"yes\" or \"no\"): ");
-		scanf("%9s", onBoard1.action);
+	printf("Warning: Stock investments might sometimes become ");
+	printf("bearish with some permanent loss.\nHaving known this,");
+	printf(" do you still want to invest? (\"yes\" or \"no\"): ");
 
-		confirm_action();
+	feedBack();
 
-		feedBack();
-
-		printf("- - - - - - - - - - -Stock Investment Company,");
-		printf(" Simply The Best!- - - - - - - - - - - - - -\n\n");
-	} while (0);
+	printf("- - - - - - - - - - - -STOCK INVESTMENT COMPANY, SIMPLY THE");
+	printf(" BEST!- - - - - - - - - - - -\n\n");
 
 	return (0);
 }
