3207e574a69705600281d98ffd3bc3a8908b0f59
Chibuike Mba <chibuike.code@gmail.com>
2023-01-19 15:56:29 +0100

    Modify header_files Directory
    In this commit, functions.h and variable.h header files were modified to
    be in tandem with the "Betty coding style for C code (adopted in
    Holberton School)" Here global variables were replaced with with local
    variables for optimization, easier maintainability and better security.
    Other changes as well as new feature additions were also implemented.

--- header_files/functions.h
+++ header_files/functions.h
@@ -1,20 +1,29 @@
 #ifndef _FUNCTIONS_H_
 #define _FUNCTIONS_H_
 
-void confirm_action(void)
+/**
+ * confirmAction - confirmation of user input
+ * @verifyAction: Struct data type used for verifying the user string input
+ *
+ * Description: This function is used to confirm the user input and can only be
+ * called to execute when the user input is a literal string "yes"
+ *
+ * Return: This function returns the literal string "yes"
+ */
+struct investor confirmAction(struct investor verifyAction)
 {
-	if ((strcmp(onBoard1.action, "yes") == 0))
+	if ((strcmp(verifyAction.action, "yes") == 0))
 	{
-		printf("\nDo you also agree with the companies' terms and conditions?"dddddd);
+		printf("\nDo you also agree with the companies' terms and conditions?");
 		printf(" (\"yes\" or \"no\"): ");
-		scanf("%9s", onBoard1.action);
+		scanf("%9s", verifyAction.action);
 	}
 	else
 	{
 		printf("\nPlease make up your mind, you can do this!\n");
 	}
 
-	if ((strcmp(onBoard1.action, "yes") == 0))
+	if ((strcmp(verifyAction.action, "yes") == 0))
 	{
 		printf("\nThank you for your confirmation, you deserve the best!\n\n");
 	}
@@ -22,55 +31,104 @@ void confirm_action(void)
 	{
 		printf("\nKindly try again\n");
 	}
+
+	return (verifyAction);
 }
 
 
-void investCommit(struct investor *onBoard2)
+/**
+ * investCommit - Accepts the capital deposit
+ * @deposit: This struct data type stores the user integer deposit amount
+ *
+ * Description: This function is used to confirm if the string literal is "yes"
+ * and then proceed to accept the capital deposit of the user
+ *
+ * Return: This function returns the capital deposit of the user
+ */
+struct investor investCommit(struct investor deposit)
 {
-	if (strcmp(onBoard2->action, "yes") == 0)
+	if (strcmp(deposit.action, "yes") == 0)
 	{
-		printf("Please, kindly input your investment capital: ");
-		scanf("%d", &onBoard2->capitalInvestment);
-		printf("\nYou have successfully invested %d", onBoard2->capitalInvestment);
-		printf(" Naira at 20%% Returns On Investment after 5 Secs\n");
+		printf("Please, enter an investment deposit between 1000 - 500000");
+		printf(" (in Dollars): ");
+		scanf("%f", &deposit.depositAmt);
 	}
+	if ((deposit.depositAmt >= 1000) && (deposit.depositAmt <= 500000))
+	{
+		printf("\nYou have successfully invested $%.2f", deposit.depositAmt);
+		printf(" at 20%% Returns On Investment after 5 Secs\n");
+	}
+	else
+	{
+		printf("\nUnsuccessful! NOTE:\nMinimum Deposit In Dollars:");
+		printf(" 1000\nMaximum Deposit in Dollars: 500000\n\n");
+	}
+	return (deposit);
 }
 
 
-void investmentDuration(struct investor *onBoard3)
+/**
+ * investmentDuration - Processes investment duration time at 5 seconds
+ *
+ * Description: This function proceses the investment duration time which is
+ * mapped to 5 seconds
+ *
+ * Return: This function returns the different in time duration of 5 seconds
+ */
+struct investor investmentDuration(void)
 {
-	thisTime = time(NULL);
+	struct investor duration;
 
-	struct tm *regionalTime = localtime(&thisTime);
+	duration.thisTime = time(NULL);
 
 	sleep(5);
 
-	after5Secs = time(NULL);
+	duration.after5Secs = time(NULL);
 
-	onBoard3->profits = 0;
+	duration.profits = 0;
 
-	onBoard3->timeDiff = difftime(after5Secs, thisTime);
+	duration.timeDiff = difftime(duration.after5Secs, duration.thisTime);
 
-	onBoard3->timeDiff = difftime(after5Secs, thisTime);
+	duration.timeDiff = difftime(duration.after5Secs, duration.thisTime);
+
+	return (duration);
 }
 
 
+/**
+ * feedBack - Calls the other functions and do some tasks
+ *
+ * Description: This function calls all the other functions above to carry out
+ * their designated tasks and then processes the ROI of the user deposit after
+ * 5 seconds as well as the sum of the user's total deposit and ROI and
+ * subsequently prints the standard output to the console.
+ */
 void feedBack(void)
 {
-	investCommit(&onBoard1);
+	struct investor confirmAct, iCommit, roiDuration, act;
 
-	investmentDuration(&onBoard1);
+	scanf("%9s", act.action);
 
-	onBoard1.profits = 0;
+	confirmAct = confirmAction(act);
 
-	if (onBoard1.timeDiff == 5)
+	iCommit = investCommit(confirmAct);
+
+	if (((strcmp(iCommit.action, "yes")) == 0) && ((iCommit.depositAmt >= 1000)
+	&& (iCommit.depositAmt <= 500000))) /* End of condition */
 	{
-		onBoard1.profits += (onBoard1.capitalInvestment * 0.2);
+		roiDuration = investmentDuration();
 
-		onBoard1.profitsAndCapital = (onBoard1.profits + onBoard1.capitalInvestment);
-		printf("\nThe total profit earned after 5 seconds plus the intial");
-		printf(" capital invested is: %d Naira\n\n", onBoard1.profitsAndCapital);
+		iCommit.profits = 0;
 
+			if (roiDuration.timeDiff == 5)
+			{
+				iCommit.profits += (iCommit.depositAmt * 0.2);
+
+				iCommit.profitsAndCapital = (iCommit.profits + iCommit.depositAmt);
+				printf("\nThe total profit earned after 5 seconds plus the intial");
+				printf(" capital invested is: $%.2f\n\n", iCommit.profitsAndCapital);
+
+			}
 	}
 }
 
--- header_files/variables.h
+++ header_files/variables.h
@@ -1,19 +1,26 @@
 #ifndef _VARIABLES_H_
 #define _VARIABLES_H_
 
+/**
+  * struct investor - Structure data type declaration
+  *
+  * @action: This 1st member stores the string literal entered by the user
+  * @depositAmt: This 2nd member stores the deposit amount entered by the user
+  * @timeDiff: This 3rd member stores the time difference of 5 seconds
+  * @profits: This 4th member stores the 20% ROI of user's deposit
+  * @profitsAndCapital: This 5th member stores the sum of (2nd + 4th) members
+  * @thisTime: This 6th member stores the current time
+  * @after5Secs: this 7th member stores the time after 5 seconds
+  */
 struct investor
-{	
+{
 	char action[10];
-	int capitalInvestment;
+	float depositAmt;
 	double timeDiff;
-	int profits;
-	int profitsAndCapital;
+	float profits;
+	float profitsAndCapital;
+	time_t thisTime;
+	time_t after5Secs;
 };
-
-struct investor onBoard1;
-
-time_t thisTime;
-
-time_t after5Secs;
 
 #endif /* _VARIABLES_H_ */
